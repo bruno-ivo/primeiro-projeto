5a5109d885ba404cc2aa2484af9f0e9b6db87cc3
bruno-ivo <73121467+bruno-ivo@users.noreply.github.com>
2021-11-09 10:32:02 -0300

    Confirmado

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'meu-primeiro-component',
+  template: `
+    <p>Meu Primeiro Component com Angular 12!</p>
+  `
+})
+export class MeuPrimeiroComponent {}
